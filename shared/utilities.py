1f1a92a3115340342378304c738e77fccf5b66ee
jasonniebauer <lookwhatjasondid@gmail.com>
2026-02-12 21:33:14 -0600

    Create employment type table function

--- shared/utilities.py
+++ shared/utilities.py
@@ -1,6 +1,29 @@
 import altair as alt
 
 
+def employment_type_table(total_employees, total_full_time, total_part_time):
+    """
+    Table for displaying employee counts by employment type.
+    
+    :param total_employees: Count of all employees
+    :param total_full_time: Count of full-time employees
+    :param total_part_time: Count of part-time employees
+    """
+    return f"""
+            <div class="table-row">
+                <span class="bold">Full-time (salaried) employees</span>
+                <span>{total_employees:,}</span>
+            </div>
+            <div class="table-row"">
+                <span class="bold">Part-time employees</span>
+                <span>{total_full_time:,}</span>
+            </div>
+            <div class="table-row">
+                <span class="bold">Total employees</span>
+                <span class="bold">{total_part_time:,}</span>
+            </div>
+            """
+
 def employment_type_pie_chart(df, ft_color, pt_color):
     """
     Pie chart for full-time vs part-time employment.
